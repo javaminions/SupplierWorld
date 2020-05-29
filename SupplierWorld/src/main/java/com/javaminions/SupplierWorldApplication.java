65703db85d5c4ca9177b2b35dbefcd5fd7a039d4
chris <chris@192.168.0.28>
2020-05-29 08:44:19 -0700

    init commit - CJ

---
+++ SupplierWorld/src/main/java/com/javaminions/SupplierWorldApplication.java
@@ -0,0 +1,13 @@
+package com.javaminions;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SupplierWorldApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SupplierWorldApplication.class, args);
+	}
+
+}
---
+++ SupplierWorld/src/test/java/com/javaminions/SupplierWorldApplicationTests.java
@@ -0,0 +1,13 @@
+package com.javaminions;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SupplierWorldApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
