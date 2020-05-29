4f480977a15a93e746917eea7f5d8c4b58e7d24e
chris <chris@192.168.0.28>
2020-05-29 12:41:48 -0700

    supplierorders pojo & repo done - CJ

--- SupplierWorld/src/main/java/com/javaminions/pojos/Supplierorders.java
+++ SupplierWorld/src/main/java/com/javaminions/pojos/Supplierorders.java
@@ -1,5 +1,51 @@
 package com.javaminions.pojos;
 
+import javax.persistence.Entity;
+
+@Entity
 public class Supplierorders {
 
+	private int id;
+	private int productcode;
+	private int quantity;
+	private String status;
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public int getProductcode() {
+		return productcode;
+	}
+	public void setProductcode(int productcode) {
+		this.productcode = productcode;
+	}
+	public int getQuantity() {
+		return quantity;
+	}
+	public void setQuantity(int quantity) {
+		this.quantity = quantity;
+	}
+	public String getStatus() {
+		return status;
+	}
+	public void setStatus(String status) {
+		this.status = status;
+	}
+	@Override
+	public String toString() {
+		return "Supplierorders [id=" + id + ", productcode=" + productcode + ", quantity=" + quantity + ", status="
+				+ status + "]";
+	}
+	public Supplierorders(int id, int productcode, int quantity, String status) {
+		super();
+		this.id = id;
+		this.productcode = productcode;
+		this.quantity = quantity;
+		this.status = status;
+	}
+	public Supplierorders() {
+		super();
+	}
 }
